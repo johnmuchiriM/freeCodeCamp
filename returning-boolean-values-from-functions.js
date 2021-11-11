ddb4b7fb41579730551d1c9180babf811f75ee25
johnmuchiriM <jeanfrancaiseharicot@gmail.com>
2021-11-11 22:20:46 +0300

    update:added a javascript page

---
+++ returning-boolean-values-from-functions.js
@@ -0,0 +1,13 @@
+function isLess(a, b) {
+  // Only change code below this line
+  // if (a < b) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
+
+  return a < b;
+  // Only change code above this line
+}
+
+isLess(10, 15);
