b81f5cdf5142ca0972c868a3753b71f615eef721
muchirimaina <60730009+muchirimaina@users.noreply.github.com>
2025-03-14 17:45:20 +0300

    Add files via upload

---
+++ Intermediate Algorithm Scripting/everything-be-true.js
@@ -0,0 +1,5 @@
+function truthCheck(collection, pre) {
+  return collection.every(obj => Boolean(obj[pre]));
+}
+
+truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot");
